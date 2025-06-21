ae7a2f03db5e04ccb46c76c5ef59097ebbff36dc
nobmurakita <nobmurakita@users.noreply.github.com>
2025-06-21 12:00:06 +0900

    「3.6 条件分岐」まで完了

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -16,8 +16,12 @@ func Eval(node ast.Node) object.Object {
 	// 文
 	case *ast.Program:
 		return evalStatements(node.Statements)
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
 
 	// 式
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
@@ -41,6 +45,31 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
+	}
 }
 
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
